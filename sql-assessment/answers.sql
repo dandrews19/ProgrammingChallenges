72fc84e68392ae329defe19a9f752ceae259c3ed
dandrews19 <75515622+dandrews19@users.noreply.github.com>
2023-09-09 11:14:20 -0700

    Create answers.sql

---
+++ sql-assessment/answers.sql
@@ -0,0 +1,56 @@
+
+
+-- 1. 
+
+SELECT date, SUM(impressions)
+FROM marketing_data
+GROUP BY date;
+
+
+-- 2.
+
+SELECT state, SUM(revenue) as total_revenue
+FROM website_revenue
+GROUP BY state
+ORDER BY total_revenue DESC
+LIMIT 3;
+
+
+-- 3.
+
+
+SELECT campaign_info.name as campaign_name, SUM(marketing_data.cost) as total_cost, SUM(marketing_data.impressions) as total_impressions, SUM(marketing_data.clicks) as total_clicks, 
+SUM(website_revenue.revenue) as total_revenue
+FROM marketing_data
+LEFT JOIN website_revenue
+ON marketing_data.campaign_id = website_revenue.campaign_id
+LEFT JOIN campaign_info
+ON marketing_data.campaign_id = campaign_info.id
+GROUP BY marketing_data.campaign_id;
+
+
+-- 4.
+
+SELECT campaign_info.name as campaign_name, SUM(marketing_data.conversions) as total_conversions, website_revenue.state 
+FROM marketing_data
+LEFT JOIN website_revenue
+ON marketing_data.campaign_id = website_revenue.campaign_id
+LEFT JOIN campaign_info
+ON marketing_data.campaign_id = campaign_info.id
+WHERE campaign_info.name = 'Campaign5'
+GROUP BY marketing_data.campaign_id, website_revenue.state
+ORDER BY total_conversions DESC;
+
+-- For Campaign5, Georgia generated the most conversions
+
+-- 5.
+
+SELECT campaign_info.name as campaign_name, (SUM(website_revenue.revenue) / SUM(marketing_data.cost)) as revenue_per_dollar_spent
+FROM marketing_data
+LEFT JOIN website_revenue ON marketing_data.campaign_id = website_revenue.campaign_id
+LEFT JOIN campaign_info ON marketing_data.campaign_id = campaign_info.id
+GROUP BY marketing_data.campaign_id
+ORDER BY revenue_per_dollar_spent DESC;
+
+
+-- Campaign4 was the most efficient because it got the most revenue per dollar spent on the campaign
